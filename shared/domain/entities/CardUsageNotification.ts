ffd33460a7d72fd9e6ae2549674048e8b14463b0
kakeru-ikeda <kakerumail1451@gmail.com>
2025-05-27 21:12:16 +0900

    feat: CardUsageNotificationDTOを新規作成し、CardUsageからの変換を定義

--- shared/domain/entities/CardUsageNotification.ts
+++
@@ -1,9 +0,0 @@
-import { CardUsage } from './CardUsage';
-
-/**
- * Discord通知用のデータモデル
- * CardUsageエンティティからcreated_atを除外し、datetime_of_useを文字列型に変更
- */
-export type CardUsageNotification = Omit<CardUsage, 'datetime_of_use' | 'created_at'> & {
-    datetime_of_use: string;
-};
